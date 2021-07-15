da4d7b1510bf321b7c80a5e07fbaa82c4e3d5833
shun713 <oizumi_shu18@yahoo.co.jp>
2021-07-15 11:41:50 +0000

    second-commit[finish]4-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -20,6 +20,16 @@ class TodolistsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
   private
     #ストロングパラメータ コントローラの中でしか呼び出せないようにする
   def list_params
